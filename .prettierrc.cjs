68b4b84c4cf4dc1885ae88de80445be1944ebe33
mm.oo.mm <goligo.lego.land@gmail.com>
2024-02-29 00:05:33 +0900

    一部修正

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,9 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  printWidth: 120, //1行の文字列を120文字にする
-  semi: true, //行末にセミコロンを追加
-  trailingComma: 'es5', //複数行の場合は可能な限り末尾のカンマを表示
-}
+  printWidth: 120, // 1行の文字列を120文字にする
+  singleQuote: true, // ダブルに代わりシングルクオーテーションを使う
+  semi: true, // 行末にセミコロンを追加
+  tabWidth: 2, // タブのスペース2
+  trailingComma: "all", // 複数行の場合は可能な限り末尾のカンマを表示
+  arrowParens: "always", // アロー関数のパラメーターをカッコで囲む
+};
