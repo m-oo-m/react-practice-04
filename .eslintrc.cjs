33c2b790619b93367ca94617ee661e9bb4c8baa9
mm.oo.mm <goligo.lego.land@gmail.com>
2024-02-29 10:12:51 +0900

    一部調整

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,47 +4,36 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    "plugin:react/recommended", // 追加
-    "plugin:react/jsx-runtime",  // 追加
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react/recommended', // 追加
+    'plugin:react/jsx-runtime', // 追加
     'plugin:react-hooks/recommended',
-    "prettier", // 追加
+    'prettier', // 追加
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh',"import"],
+  plugins: ['react-refresh', 'import'],
   settings: {
-    react: { version: "detect" }, // 追加
+    react: { version: 'detect' }, // 追加
   },
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    // import の並び順を設定
+    'import/order': [
       'warn',
-      { allowConstantExport: true },
-    ],
-     // import の並び順を設定
-     "import/order": [
-      "warn",
       {
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          ["parent", "sibling"],
-          "object",
-          "type",
-          "index",
-        ],
-        "newlines-between": "always",
-        pathGroupsExcludedImportTypes: ["builtin"],
-        alphabetize: { order: "asc", caseInsensitive: true },
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'object', 'type', 'index'],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
         pathGroups: [
           {
-            pattern: "react",
-            group: "external",
-            position: "before",
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
           },
         ],
       },
     ],
   },
-}
+};
